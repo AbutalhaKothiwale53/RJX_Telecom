6bed0e3cab51f25bdc4d87bb2fb2ff368bbb1626
Abutalha Kothiwale <70056766+AbutalhaKothiwale53@users.noreply.github.com>
2023-03-04 21:11:45 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import addPlan from 'AddPlan';
-import viewPlan from 'ViewPlan';  
+import { BrowserRouter,Link, Route, Routes } from 'react-router-dom';
+import AddPlan from './AddPlan';
+import ViewPlan from './ViewPlan'; 
 
 function App() {
   return (
     <React.Fragment>
-    <BrowserRouter>
-    <div className="container-fluid">
-    <nav className='navbar navbar-expand-lg navbar-light bg-custom'>
-    <span>Telecall Prepaid</span>
-    <ul>
-    <li><Link to='addPan'>
-    Add Plan</Link></li>
-    <li><Link to='viewPlan' >View Plan</Link></li>
-    </ul>
-    </nav>
-      <Routes>
-      <Route path='/addPlan' element={<AddPlan />} />
-      <Route path='/viewPlan' element={<ViewPlan />} />
-      </Routes>
-    </div>
-    </BrowserRouter>
-    <React.Fragment>
+            <BrowserRouter>
+            <div className="container-fluid">
+                <nav className='navbar navbar-expand-lg navbar-light bg-custom'>
+                <span>Telecall Prepaid</span>
+                <ul>
+                    <li><Link to='addPlan'>Add Plan</Link></li>
+                    <li><Link to='viewPlan'>View Plan</Link></li>
+                </ul>
+                </nav>
+                
+                <Routes>
+                    <Route excat path="/" element={<AddPlan />} />
+                <Route path='/AddPlan' element={<AddPlan />} />
+                <Route path='/ViewPlan' element={<ViewPlan />} />
+                </Routes>
+            </div>
+            </BrowserRouter>
+       </React.Fragment>
   );
 }
 
