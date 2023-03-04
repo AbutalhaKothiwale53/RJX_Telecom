89094e9726732664e01651b56e2fd801a8735432
Abutalha Kothiwale <70056766+AbutalhaKothiwale53@users.noreply.github.com>
2023-03-04 21:10:59 +0530

    Update ViewPlan.js

--- ViewPlan.js
+++ ViewPlan.js
@@ -15,7 +15,7 @@ export default function ViewPlan() {
 	});
 
 	//useEffect can be used if required to fetch the plan details when the component is mounted
-	functiono fetchDetails() {
+	function fetchDetails() {
 		//axios call be made here for fetching details
 		//success -> set the state planDetails with the response received.
 		//error -> set the error message as "Something went wrong"
@@ -28,29 +28,32 @@ export default function ViewPlan() {
 	}
 
 	return (
-		<React.Fragment>
-			{/*display message based on condition */}
+        <React.Fragment>
+            {/*display message based on condition */}
 			<p>{/*display successMessage*/}</p>
 			<p>{/*display errorMessage*/}</p>
-			<h6>Plan Details</h6>
-			<div className='row'>
-				<div key="plan.id" className='card'>
-				<div className='card-body'>
-				<b>Plan ID: </b>
-				<span>{/*id*/}</span></br>
-				<b>Plan Value: </b>
-				<span>{/* planValue*/}</span></br>
-				<b>Data: </b>
-				<span>{/* data */}</span></br>
-				<b>Unlimited Calls: </b>
-				<span>{/*calls*/}</span></br>
-				<b>AddOns: </b>
-				<span>{/*addOns*/}</span></br>   
-				</div>
-				</div>
-				<button type='submit'>Delete</button>
-			</div>
+            <h4>Plan Details</h4>
+            <div className="row">
+                <div key='plan.id' className='card'>
+                    <div className='card-body'>
+                    <b>Plan ID: </b>
+                    <span>{/*id */}</span><br/><br/>
 
-		</React.Fragment>
+                    <b>Plan Value: </b>
+                    <span>{/*value */}</span><br/><br/>
+
+                    <b>Data: </b>
+                    <span>{/*data */}</span><br/><br/>
+
+                    <b>Unlimited Calls: </b>
+                    <span>{/*calls*/}</span><br/><br/>
+
+                    <b>AddOns: </b>
+                    <span>{/*addOns */}</span>
+                    </div>
+                </div>
+                <button type='submit' >Delete</button>
+            </div>
+        </React.Fragment>
 	);
 }  
